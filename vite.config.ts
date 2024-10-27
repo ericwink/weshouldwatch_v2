a024a992e7d0a8cfef2b9e7f61bacd98c6ee7b3a
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2024-10-26 22:04:19 -0400

    add vinxi

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+import { createApp } from "vinxi";
+import { config } from "vinxi/plugins/config";
+
+export default createApp({
+  routers: [
+    {
+      name: "public",
+      type: "static",
+      dir: "./public",
     },
-  },
+    {
+      name: "client",
+      type: "spa",
+      handler: "./index.html",
+      base: "/",
+      plugins: () => [
+        tsconfigPaths(),
+        config("vite", {
+          // additional vite options
+          plugins: [react()],
+        }),
+        // additional vite plugins
+      ],
+    },
+  ],
 });
